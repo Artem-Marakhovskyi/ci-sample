6591f95618077a512d9ee8c32bab9ebd292b64ee
Artem Marakhovskyi <Artem.marakhovskyi@gmail.com>
2019-01-02 00:23:37 +0200

    projects are added. on netcore proj tests are not visible

---
+++ EmptyProject.Tests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace EmptyProject.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void A()
+        {
+            Assert.True(true);
+        }
+    }
+}
